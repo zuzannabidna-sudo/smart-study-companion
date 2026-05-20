fa27e4b3165d8f542b217bac8b2b16b809661b7c
zuzannabidna-sudo <zuzannabidna@gmail.com>
2026-05-20 20:42:02 +0200

    minSdk has been changed to 31

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,7 +10,7 @@ android {
 
     defaultConfig {
         applicationId = "com.example.obrona"
-        minSdk = 24
+        minSdk = 31
         targetSdk = 36
         versionCode = 1
         versionName = "1.0"
